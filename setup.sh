4a0af0355db9b42e46835603301f92e962ffa7cf
Cameron Eagans <cweagans@gmail.com>
2014-07-28 13:39:15 -0600

    Add git config

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ cd ~
 [ -e ~/.offlineimaprc ] || ln -s ~/dotfiles/offlineimaprc ~/.offlineimaprc
 [ -e ~/.muttrc ] || ln -s ~/dotfiles/muttrc ~/.muttrc
 [ -e ~/.msmtprc ] || ln -s ~/dotfiles/msmtprc ~/.msmtprc
+[ -e ~/.gitconfig ] || ln -s ~/dotfiles/gitconfig ~/.gitconfig
+[ -e ~/.gitignore_global ] || ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 [ -e ~/.bash_profile ] || ln -s ~/dotfiles/bash_profile ~/.bash_profile
 [ -e ~/Library/LaunchAgents/offlineimap-quickfetch.plist ] || ln -s ~/dotfiles/offlineimap-quickfetch.plist ~/Library/LaunchAgents/offlineimap-quickfetch.plist
 
