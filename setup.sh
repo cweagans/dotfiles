34b159f124fc5c98ceb274c1c662aa01b56e2178
Cameron Eagans <cweagans@gmail.com>
2015-07-22 16:59:47 -0600

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 # Install dependencies
-brew install offlineimap sqlite mutt contacts msmtp
+brew install offlineimap sqlite mutt w3m libntlm gsasl
+brew install msmtp --build-from-source
 
 cd ~
-[ -d ~/.mail ] || mkdir ~/.mail
+[ -d ~/Mail ] || mkdir ~/Mail
 [ -d ~/.mutt ] || mkdir -p ~/.mutt/alias ~/.mutt/cache/headers ~/.mutt/cache/bodies ~/.mutt/certificates ~/.mutt/mailcap ~/.mutt/temp ~/.mutt/sig
-[ -e ~/.offlineimaprc ] || ln -s ~/dotfiles/offlineimaprc ~/.offlineimaprc
-[ -e ~/.muttrc ] || ln -s ~/dotfiles/muttrc ~/.muttrc
-[ -e ~/.msmtprc ] || ln -s ~/dotfiles/msmtprc ~/.msmtprc
-[ -e ~/.gitconfig ] || ln -s ~/dotfiles/gitconfig ~/.gitconfig
-[ -e ~/.gitignore_global ] || ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
-[ -e ~/.bash_profile ] || ln -s ~/dotfiles/bash_profile ~/.bash_profile
-[ -e ~/.drushrc.php ] || ln -s ~/dotfiles/drushrc.php ~/.drushrc.php
-[ -e ~/Library/LaunchAgents/offlineimap-quickfetch.plist ] || ln -s ~/dotfiles/offlineimap-quickfetch.plist ~/Library/LaunchAgents/offlineimap-quickfetch.plist
-
+[ -e ~/.offlineimaprc ] || ln -s ~/.dotfiles/offlineimaprc ~/.offlineimaprc
+[ -e ~/.muttrc ] || ln -s ~/.dotfiles/muttrc ~/.muttrc
+[ -e ~/.msmtprc ] || ln -s ~/.dotfiles/msmtprc ~/.msmtprc
+[ -e ~/.gitconfig ] || ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+[ -e ~/.gitignore_global ] || ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
+[ -e ~/Library/LaunchAgents/offlineimap-quickfetch.plist ] || ln -s ~/.dotfiles/offlineimap-quickfetch.plist ~/Library/LaunchAgents/offlineimap-quickfetch.plist
