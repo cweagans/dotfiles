c072084948c7c8eb9479adb03e7b1abec87a4b39
Cameron Eagans <me@cweagans.net>
2016-12-07 18:37:06 -0700

    Add atom packages and configuration

---
+++ .dotfiles.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# <Atom>
+
+## Visuals
+apm install nucleus-dark-ui behave-theme
+
+## General
+apm install editorconfig file-icons zentabs minimap auto-update-plus
+
+## Vim
+apm install vim-mode-plus vim-mode-plus-ex-mode vim-mode-plus-subword-movement
+
+## Language support
+apm install language-docker language-terraform php-integrator-base php-integrator-navigation php-integrator-tooltips php-integrator-annotations
+
+## Linting
+## Other linters available here: http://atomlinter.github.io/
+apm install linter linter-docker linter-terraform-syntax php-integrator-linter
+
+## Autocomplete
+apm install php-integrator-autocomplete-plus
+
+## Non-code editing
+apm install nvatom tasks language-eml linter-write-good linter-retext
+
+# </Atom>
