dd743f8b03f426b93808fadb5e72cef6201011fe
Cameron Eagans <me@cweagans.net>
2017-06-19 20:52:12 -0600

    Add Homebrew path to list

--- .config/zsh/conf/env.zsh
+++ .config/zsh/conf/env.zsh
@@ -2,7 +2,7 @@ export GOPATH=~/Code/go
 export EDITOR="gvim -f"
 
 # Auto-$PATH.
-for dir in $HOME/bin $HOME/.config/composer/vendor/bin $GOPATH/bin
+for dir in /usr/local/bin $HOME/bin $HOME/.config/composer/vendor/bin $GOPATH/bin
 do
 	# @TODO: Check to see if $dir is already in $PATH first.
 	if [ -e $dir ]; then
