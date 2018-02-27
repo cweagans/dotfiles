fcebb0fe9cfb85b6ea9a568025b6f21c7d6d29bb
Cameron Eagans <me@cweagans.net>
2018-02-27 11:27:24 -0700

    Add screen lock shortcut

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -11,3 +11,8 @@ spoon.ReloadConfiguration:start()
 -- Caffeinate toggle in menu bar.
 spoon.Caffeine:start()
 
+-- Add a screen lock shortcut.
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'L', function()
+  hs.caffeinate.lockScreen()
+end)
+
