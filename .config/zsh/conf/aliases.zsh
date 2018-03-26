a9694c3911b3ca57fa9b1d6e312e931d80f64e17
Cameron Eagans <me@cweagans.net>
2018-03-25 21:29:03 -0600

    Add drud aliases

--- .config/zsh/conf/aliases.zsh
+++ .config/zsh/conf/aliases.zsh
@@ -8,3 +8,8 @@ alias ll='ls'
 # Never actually delete anything. Move it to the Trash first.
 alias gnurm='/usr/bin/rm'
 alias rm='trash'
+
+# Drud stuff
+alias dddev="~/Code/go/src/github.com/drud/ddev/bin/darwin/darwin_amd64/ddev"
+alias project-manager="~/Code/go/src/github.com/drud/hosting/project-manager/bin/darwin/darwin_amd64/project-manager"
+alias site-manager="~/Code/go/src/github.com/drud/hosting/site-manager/bin/darwin/darwin_amd64/site-manager"
