ad03fc4aa5258babae2f5d0a58d976a18ae12cc3
Cameron Eagans <me@cweagans.net>
2018-04-26 14:30:59 -0600

    Strip out macos specific stuff

--- .config/zsh/conf/aliases.zsh
+++ .config/zsh/conf/aliases.zsh
@@ -10,6 +10,6 @@ alias gnurm='/usr/bin/rm'
 alias rm='trash'
 
 # Drud stuff
-alias dddev="~/Code/go/src/github.com/drud/ddev/bin/darwin/darwin_amd64/ddev"
-alias project-manager="~/Code/go/src/github.com/drud/hosting/project-manager/bin/darwin/darwin_amd64/project-manager"
-alias site-manager="~/Code/go/src/github.com/drud/hosting/site-manager/bin/darwin/darwin_amd64/site-manager"
+alias dddev="~/Code/go/src/github.com/drud/ddev/bin/linux/ddev"
+alias project-manager="~/Code/go/src/github.com/drud/hosting/project-manager/bin/linux/project-manager"
+alias site-manager="~/Code/go/src/github.com/drud/hosting/site-manager/bin/linux/site-manager"
--- .config/zsh/conf/env.zsh
+++ .config/zsh/conf/env.zsh
@@ -1,22 +1,8 @@
 export GOPATH=~/Code/go
-export EDITOR="gvim -f"
+export EDITOR="subl -w"
 
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 
-if [[ -x "$(which launchctl)" ]]
-then
-	launchctl setenv XDG_CACHE_HOME $XDG_CACHE_HOME
-	launchctl setenv XDG_CONFIG_HOME $XDG_CONFIG_HOME
-	launchctl setenv XDG_DATA_HOME $XDG_DATA_HOME
-fi
-
-# Auto-$PATH.
-for dir in /usr/local/bin $HOME/bin $HOME/.config/composer/vendor/bin $GOPATH/bin
-do
-	# @TODO: Check to see if $dir is already in $PATH first.
-	if [ -e $dir ]; then
-		export PATH=$dir:$PATH
-	fi
-done
+export PATH=$HOME/bin:$HOME/.config/composer/vendor/bin:$GOPATH/bin:$PATH
