cc47909ddd3945acc9a1e749f9b98f7a35d47eb8
Cameron Eagans <me@cweagans.net>
2018-07-04 00:09:12 -0600

    Clean up package installation stuff

--- .yadm/scripts/install-system-deps.sh
+++ .yadm/scripts/install-system-deps.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 # If arch linux:
-	# make sure that yay is installed
 	# install pacman deps
 	# install yay deps
+	# make sure multilib is enabled to be able to install wine
 
+packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python-neovim sudo trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
+
+aur_packages="ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop snowboard spotify ttf-input typora yay-bin yadm-git zoom"
+
+echo " ---> Updating package information"
+sudo pacman -Sy
+
+echo " ---> Installing/updating Arch packages"
+sudo pacman -S $packages
+
+echo " ---> Installing AUR packages"
+yay -S $aur_packages
