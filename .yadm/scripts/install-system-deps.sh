bd7c3443e75213024dbd89dc49bc3f809ceca536
Cameron Eagans <me@cweagans.net>
2018-07-03 23:27:53 -0600

    strip out more mac stuff + install vigor on bootstrap

---
+++ .yadm/scripts/install-system-deps.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# If arch linux:
+	# make sure that yay is installed
+	# install pacman deps
+	# install yay deps
+
--- .yadm/scripts/zsh-default-shell.sh
+++ .yadm/scripts/zsh-default-shell.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-# Add zsh to /etc/shells if it's not already there.
-if grep -xq "/usr/local/bin/zsh" /etc/shells ; then
-	echo "Homebrew-installed zsh is already in /etc/shells."
-else
-	sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
-fi
-
-# @TODO: Only do this if $SHELL != zsh
-chsh -s /usr/local/bin/zsh
-
+chsh -s /usr/bin/zsh
