d1e29aac086e10064ab111c819dfdbe9cab0ceb1
Cameron Eagans <me@cweagans.net>
2018-07-05 12:28:22 -0600

    Add vale-bin to the list of AUR packages to install

--- .yadm/scripts/install-system-deps.sh
+++ .yadm/scripts/install-system-deps.sh
@@ -5,9 +5,9 @@
 	# install yay deps
 	# make sure multilib is enabled to be able to install wine
 
-packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python-neovim sudo trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
+packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python-neovim sudo texlive-core trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
 
-aur_packages="ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop snowboard spotify ttf-input typora yay-bin yadm-git zoom"
+aur_packages="ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop snowboard spotify ttf-input typora vale-bin yay-bin yadm-git zoom"
 
 echo " ---> Updating package information"
 sudo pacman -Sy
