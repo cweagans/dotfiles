1b7a3aba9c5b0acdd60f977ac4c9c084c0bd7242
Cameron Eagans <me@cweagans.net>
2018-07-05 12:36:09 -0600

    Add vim-vint

--- .yadm/scripts/install-system-deps.sh
+++ .yadm/scripts/install-system-deps.sh
@@ -5,9 +5,11 @@
 	# install yay deps
 	# make sure multilib is enabled to be able to install wine
 
-packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python-neovim sudo texlive-core trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
+packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python python-neovim python-pip sudo texlive-core trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
 
 aur_packages="ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop snowboard spotify ttf-input typora vale-bin yay-bin yadm-git zoom"
+
+python_packages="vint"
 
 echo " ---> Updating package information"
 sudo pacman -Sy
@@ -17,3 +19,6 @@ sudo pacman -S $packages
 
 echo " ---> Installing AUR packages"
 yay -S $aur_packages
+
+echo " ---> Installing Python packages"
+pip install vim-vint
