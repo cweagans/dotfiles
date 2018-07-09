e1200de9d0c7b9c4ffd96fbbb47abd3f72b23150
Cameron Eagans <me@cweagans.net>
2018-07-09 09:42:54 -0600

    Remove snowboard and drafter

--- .yadm/scripts/install-system-deps.sh
+++ .yadm/scripts/install-system-deps.sh
@@ -7,7 +7,7 @@
 
 packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python python-neovim python-pip sudo texlive-core trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
 
-aur_packages="ddev-bin drafter dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop snowboard spotify ttf-input typora vale-bin yay-bin yadm-git zoom"
+aur_packages="ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop spotify ttf-input typora vale-bin yay-bin yadm-git zoom"
 
 python_packages="vint"
 
