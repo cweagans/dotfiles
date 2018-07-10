ef79cd39602cd4b5592ff3777c481b4123fbe782
Cameron Eagans <me@cweagans.net>
2018-07-10 09:45:13 -0600

    Add alacritty-git

--- .yadm/scripts/install-system-deps.sh
+++ .yadm/scripts/install-system-deps.sh
@@ -5,9 +5,10 @@
 	# install yay deps
 	# make sure multilib is enabled to be able to install wine
 
+# todo: remove kitty once alacritty is configured.
 packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python python-neovim python-pip sudo texlive-core trash-cli vlc w3m wget wine wine-mono wine_gecko xclip zsh"
 
-aur_packages="ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop spotify ttf-input typora vale-bin yay-bin yadm-git zoom"
+aur_packages="alacritty-git ddev-bin dropbox kubernetes-helm kubernetes-kubectl neovim-gtk slack-desktop spotify ttf-input typora vale-bin yay-bin yadm-git zoom"
 
 python_packages="vint"
 
