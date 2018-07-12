1ab121256f66527e65a723b59326b868c6a4cc66
Cameron Eagans <me@cweagans.net>
2018-07-11 21:05:40 -0600

    Remove aliases. I have these in ~/bin now

--- .config/zsh/conf/aliases.zsh
+++ .config/zsh/conf/aliases.zsh
@@ -8,8 +8,3 @@ alias ll='ls'
 # Never actually delete anything. Move it to the Trash first.
 alias gnurm='/usr/bin/rm'
 alias rm='trash'
-
-# Drud stuff
-alias dddev="~/Code/go/src/github.com/drud/ddev/bin/linux/ddev"
-alias project-manager="~/Code/go/src/github.com/drud/hosting/project-manager/bin/linux/project-manager"
-alias site-manager="~/Code/go/src/github.com/drud/hosting/site-manager/bin/linux/site-manager"
