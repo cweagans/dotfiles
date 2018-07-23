d3251e6e6db25a36a16ef40a5af53ca768f186cd
Cameron Eagans <me@cweagans.net>
2018-07-23 11:17:36 -0600

    Notify when there are new messages

---
+++ .config/offlineimap/post-sync.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Index mail with notmuch.
+notmuch new
+
+# Notify on new mail.
+ACCOUNT=$1
+NEWMSGS=$(/bin/ls ~/Mail/$ACCOUNT/INBOX/new | wc -l)
+notify-send --app-name="OfflineIMAP" "New mail" "$NEWMSGS new messages in $ACCOUNT"
+
