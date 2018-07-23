e522f438fe64bdee9c9f7a9386dd398bf2596e13
Cameron Eagans <me@cweagans.net>
2018-07-23 12:51:52 -0600

    Only notify if there are actually new messages

--- .config/offlineimap/post-sync.sh
+++ .config/offlineimap/post-sync.sh
@@ -6,5 +6,7 @@ notmuch new
 # Notify on new mail.
 ACCOUNT=$1
 NEWMSGS=$(/bin/ls ~/Mail/$ACCOUNT/INBOX/new | wc -l)
-notify-send --app-name="OfflineIMAP" "New mail" "$NEWMSGS new messages in $ACCOUNT"
+if [ "$NEWMSGS" != "0" ]; then
+	notify-send --app-name="OfflineIMAP" "New mail" "$NEWMSGS new messages in $ACCOUNT"
+fi
 
