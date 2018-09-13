97b5e745f27befd56ccbb6fc8761d762a568eceb
Cameron Eagans <me@cweagans.net>
2018-09-13 15:52:02 -0600

    install vscode

--- .yadm/scripts/install-system-deps.sh
+++ .yadm/scripts/install-system-deps.sh
@@ -8,7 +8,7 @@
 # todo: remove kitty once alacritty is configured.
 packages="ansible arc-gtk-theme arc-icon-theme asciidoc blender composer cups dep docker feedreader firefox git go hub khal khard kitty libreoffice-fresh msmtp mutt neovim notmuch noto-fonts noto-fonts-cjk offlineimap pandoc pinta python python-neovim python-pip sudo texlive-core trash-cli vdirsyncer vlc w3m wget wine wine-mono wine_gecko xclip zsh"
 
-aur_packages="alacritty-git ddev-bin dropbox intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre keybase-bin kubernetes-helm kubectl-bin neovim-gtk slack-desktop spotify ttf-input typora vale-bin yay-bin yadm-git zazu-bin zoom"
+aur_packages="alacritty-git ddev-bin dropbox intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre keybase-bin kubernetes-helm kubectl-bin neovim-gtk slack-desktop spotify ttf-input typora vale-bin visual-studio-code-bin yay-bin yadm-git zazu-bin zoom"
 
 python_packages="vint"
 
@@ -23,6 +23,18 @@ yay -S $aur_packages
 
 echo " ---> Installing Python packages"
 pip install vim-vint
+
+echo " ---> Installing vscode packages"
+code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension EditorConfig.EditorConfig
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension humao.rest-client
+code --install-extension mikestead.dotenv
+code --install-extension ms-vscode.Go
+code --install-extension redhat.vscode-yaml
+code --install-extension vscodevim.vim
+code --install-extension vscoss.vscode-ansible
+code --install-extension wholroyd.jinja
 
 echo " ---> Making sure that khard/khal/vdirsyncer have work dirs"
 [ -d ~/.cache/vdirsyncer ] || mkdir ~/.cache/vdirsyncer
