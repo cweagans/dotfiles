7e2b07b65fb024235abf15af85193514381e687b
Cameron Eagans <me@cweagans.net>
2018-09-13 15:54:44 -0600

    Remove vigor

--- .yadm/scripts/vim-vigor.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-[ -d ~/.config/nvim ] || git clone git@github.com:vim-vigor/vigor.git ~/.config/nvim
-for p in language-prose vigor-ui linting git language-go language-vim completion defaults; do
-	echo " ---> Ensuring that the ${p} layer is installed."
-	[ -d ~/.config/nvim/layers/${p} ] || git clone git@github.com:vim-vigor/${p}.git ~/.config/nvim/layers/${p}
-done
