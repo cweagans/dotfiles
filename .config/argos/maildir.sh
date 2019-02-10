e7cc4551b1fb0d0e6440dcc207b0f991cfd17442
Cameron Eagans <me@cweagans.net>
2019-02-09 18:52:28 -0700

    Show count of all messages, not just unread

--- .config/argos/maildir.sh
+++ .config/argos/maildir.sh
@@ -4,8 +4,8 @@
 # Shows total unread messages in Gnome top bar + a breakdown of each mailbox.
 ##
 
-echo "📥 `find ~/.cache/mail/*/Inbox/new/ -type f | wc -l`"
+echo "📥 `find ~/.cache/mail/*/Inbox/{new,cur}/ -type f | wc -l`"
 echo "---"
 for d in ~/.cache/mail/*/; do
-    echo "`basename $d`: `find $d/Inbox/new/ -type f | wc -l`"
+    echo "`basename $d`: `find $d/Inbox/{new,cur}/ -type f | wc -l`"
 done
