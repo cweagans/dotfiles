ff146a9cdab730e512c43d108c93dbe5dc02a32b
Cameron Eagans <me@cweagans.net>
2019-02-16 14:39:53 -0700

    Fix syntax error

--- .config/mutt/account-select.sh
+++ .config/mutt/account-select.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ `date +"%H"` -lt 18 ] && [ `date +"%H"` -ge 07 && [ `date +"%u"` != 6 ] && [ `date +"%u"` != 7 ]; then
+if [ `date +"%H"` -lt 18 ] && [ `date +"%H"` -ge 07 ] && [ `date +"%u"` != 6 ] && [ `date +"%u"` != 7 ]; then
     echo 'source ~/.config/mutt/accounts/digitaldeployment.muttrc'
 else
     echo 'source ~/.config/mutt/accounts/gmail.muttrc'
