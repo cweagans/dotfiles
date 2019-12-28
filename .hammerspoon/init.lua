ca3d69c7bdc4e14e75d50738840501e39b3a9dab
Cameron Eagans <me@cweagans.net>
2019-12-27 19:45:20 -0700

    Add slow quit

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -24,6 +24,22 @@ ht.keywords = {
 }
 ht:start()
 
+-- Slow quit (replaces CommandQ)
+-- Taken from https://github.com/dbmrq/dotfiles/blob/master/home/.hammerspoon/slowq.lua
+local killedIt = false
+function pressedQ()
+    killedIt = false
+    hs.alert.show("CmdQ")
+    hs.timer.usleep(1000000 * .1)
+end
+function repeatQ()
+    if killedIt then return end
+    hs.application.frontmostApplication():kill()
+    killedIt = true
+    hs.alert.closeAll()
+end
+hs.hotkey.bind('cmd', 'Q', pressedQ, nil, repeatQ)
+
 -- Load, configure, and start Magneto.
 m = hs.loadSpoon("Magneto")
 m:start()
