9436af08090b1e7e66062436381e9dd110c33dc5
Cameron Eagans <me@cweagans.net>
2020-01-04 17:10:11 -0700

    disable key repeat

--- .config/yadm/scripts/mac-defaults.sh
+++ .config/yadm/scripts/mac-defaults.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 echo " -> Configuring mac settings"
+
+echo "   - Disabling press and hold so that keys will repeat as expected"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
