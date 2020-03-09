25dcfb9d36084a36291a040765568aeda080ed06
Cameron Eagans <me@cweagans.net>
2020-03-09 10:53:35 -0600

    Add mysql-client path to PATH

--- .config/zsh/conf/env.zsh
+++ .config/zsh/conf/env.zsh
@@ -6,6 +6,7 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 
 export PATH=$HOME/bin:$HOME/.config/composer/vendor/bin:$GOPATH/bin:$HOME/Code/npm/bin:$HOME/Code/flutter/bin:$PATH
+export PATH=/usr/local/opt/mysql-client/bin:$PATH
 
 # This is needed for yadm encrypt to work on mac for some reason?
 # https://github.com/TheLocehiliosan/yadm/issues/33#issuecomment-274167682
