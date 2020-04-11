90764db6146383a92e1c7315e36744e3cabcd916
Cameron Eagans <me@cweagans.net>
2020-04-10 22:14:55 -0600

    Remove magneto and add keybindings for streaming

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -20,10 +20,6 @@ hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'L', function()
     hs.caffeinate.lockScreen()
 end)
 
--- Load, configure, and start Magneto.
-m = hs.loadSpoon("Magneto")
-m:start()
-
 -- Ctrl + Space should toggle kitty (terminal) visibility.
 hs.hotkey.bind({"ctrl"}, "space", function()
     local app = hs.application.get("kitty")
@@ -40,3 +36,59 @@ hs.hotkey.bind({"ctrl"}, "space", function()
     end
 end)
 
+-- Ctrl + Alt + D, F, G should position a window half left, full screen, or half
+-- right for streaming. All coordinates are for my 32:9 monitor and YMMV.
+-- Ctrl + Alt + E, T should be offscreen left or right respectively.
+hs.hotkey.bind({"ctrl", "alt"}, "d", function()
+    moveWindow("streamLeft")
+end)
+hs.hotkey.bind({"ctrl", "alt"}, "f", function()
+    moveWindow("streamFull")
+end)
+hs.hotkey.bind({"ctrl", "alt"}, "g", function()
+    moveWindow("streamRight")
+end)
+hs.hotkey.bind({"ctrl", "alt"}, "e", function()
+    moveWindow("offscreenLeft")
+end)
+hs.hotkey.bind({"ctrl", "alt"}, "t", function()
+    moveWindow("offscreenRight")
+end)
+
+
+function moveWindow(position)
+    -- Get the current window as long as it's standard.
+    local currentWindow = hs.window.focusedWindow()
+    if not currentWindow:isStandard() then
+        hs.logger.i("Cannot manipulate current window")
+        return nil
+    end
+
+    newRect = {}
+    newRect.y = 23    -- accounts for macOS menu bar. I always have it displaying.
+
+    if position == "streamLeft" then
+        newRect.x = 1280
+        newRect.w = 1280
+        newRect.h = 1920
+    elseif position == "streamFull" then
+        newRect.x = 1280
+        newRect.w = 2560
+        newRect.h = 1920
+    elseif position == "streamRight" then
+        newRect.x = 2560
+        newRect.w = 1280
+        newRect.h = 1920
+    elseif position == "offscreenLeft" then
+        newRect.x = 0
+        newRect.w = 1280
+        newRect.h = 1920
+    elseif position == "offscreenRight" then
+        newRect.x = 3840
+        newRect.w = 1280
+        newRect.h = 1920
+    end
+
+    currentWindow:setFrame(newRect, 0)
+
+end
