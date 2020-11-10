0e2badc0dbd944b1ff16f53f7b13019f3d709a43
Cameron Eagans <me@cweagans.net>
2020-11-10 15:01:33 -0700

    New PATH setting method

--- .config/zsh/conf/env.zsh
+++ .config/zsh/conf/env.zsh
@@ -5,15 +5,7 @@ export XDG_CACHE_HOME=$HOME/.cache
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 
-# If our $PATH doesn't already include composer, then we need to set it manually.
-# This file is executed on startup by launchd to set the environment for all GUI
-# applications as well, so it's important that this is done conditionally so that
-# the path on the terminal doesn't double-set the path.
-if [[ "$PATH" != *"composer"* ]]; then
-	export PATH=$HOME/bin:$HOME/.config/composer/vendor/bin:$GOPATH/bin:$HOME/Code/npm/bin:$HOME/Code/flutter/bin:$PATH
-	export PATH=/usr/local/opt/mysql-client/bin:$PATH
-	export PATH=/usr/local/opt/llvm/bin:$PATH
-fi
+# $PATH shouldn't be set in this file. See ~/.config/yadm/scripts/mac-paths.sh
 
 # This is needed for yadm encrypt to work on mac for some reason?
 # https://github.com/TheLocehiliosan/yadm/issues/33#issuecomment-274167682
