f846f2f906a8b0cb14f8b2cd42450fe6c5eb9be0
Cameron Eagans <me@cweagans.net>
2022-04-04 17:43:27 -0600

    Get inbox count from apple mail

--- .hammerspoon/mail.lua
+++ .hammerspoon/mail.lua
@@ -1,40 +1,22 @@
 
-local mailPath = "~/Mail"
-local inboxPaths = mailPath .. '/*/Inbox/{new,cur}/'
-local outboxPath = mailPath .. "/Outbox"
-
-local outboxCount = 0;
 local inboxCount = 0;
 local mailMenuBar = hs.menubar.new()
 
 function countInboxMessages()
-    local output, status, type, rc = hs.execute('find ' .. inboxPaths ..  ' -type f | wc -l | xargs')
+    local output, status, type, rc = hs.execute('osascript -e \'tell Application "Mail"\' -e \'unread count of inbox\' -e \'end tell\'')
     return tonumber(output)
-end
-
-function countOutboxMessages()
-    local output, status, type, rc = hs.execute('find ' .. outboxPath .. ' -type d | wc -l | xargs')
-    local count = tonumber(output)
-    count = count - 1
-    return count
 end
 
 function updateMenuBar()
     inboxCount = countInboxMessages()
-    outboxCount = countOutboxMessages()
 
     local title = ''
-    title = title .. 'I: ' .. inboxCount
-    if outboxCount > 0 then
-        title = title .. ' O: ' .. outboxCount
+    if inboxCount > 0 then
+        title = title .. '📬 ' .. inboxCount
     end
 
     mailMenuBar:setTitle(title)
 end
-
-hs.pathwatcher.new(mailPath, function(paths, flagTables)
-    updateMenuBar()
-end):start()
 
 mailMenuBar:setClickCallback(updateMenuBar)
 updateMenuBar()
