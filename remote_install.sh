9cc1137a5f1bf8413b77124302164df13ba0e5af
Cameron Eagans <me@cweagans.net>
2022-12-20 15:00:19 -0700

    Set up bitwarden session in bootstrap so chezmoi can access it

--- remote_install.sh
+++ remote_install.sh
@@ -10,9 +10,19 @@ if [[ "$(uname)" == "Darwin" ]]; then
 	# Install chezmoi and Bitwarden CLI (for secret storage).
 	brew install bitwarden-cli chezmoi
 
+	# Set up Bitwarden session so that Chezmoi has access.
+	echo "Login to Bitwarden:"
+	while ! bw login --check > /dev/null 2>&1; do
+		bw login
+	done
+	export BW_SESSION="$(bw unlock --raw)"
+
 	# Bootstrap environment (will prompt for values in chezmoi config).
 	chezmoi init --apply --verbose https://github.com/cweagans/dotfiles.git
 
 	# Fix git remote for dotfiles repo.
 	git -C $(chezmoi source-path) remote set-url origin git@github.com:cweagans/dotfiles.git
+
+	# Clear Bitwarden session.
+	unset BW_SESSION
 fi
