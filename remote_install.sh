c519a84e4982ae99cc38bd8c2a1761ce4b4e7c14
Cameron Eagans <me@cweagans.net>
2022-12-20 16:38:12 -0700

    chezmoi will automatically prompt for bitwarden credentials if necessary, so this is not needed

--- remote_install.sh
+++ remote_install.sh
@@ -10,13 +10,6 @@ if [[ "$(uname)" == "Darwin" ]]; then
 	# Install chezmoi and Bitwarden CLI (for secret storage).
 	brew install bitwarden-cli chezmoi
 
-	# Set up Bitwarden session so that Chezmoi has access.
-	echo "Login to Bitwarden:"
-	while ! bw login --check > /dev/null 2>&1; do
-		bw login
-	done
-	export BW_SESSION="$(bw unlock --raw)"
-
 	# Bootstrap environment (will prompt for values in chezmoi config).
 	chezmoi init --apply --verbose https://github.com/cweagans/dotfiles.git
 
