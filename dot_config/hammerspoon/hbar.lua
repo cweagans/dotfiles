16267b49a2652291d70157c1d8fdcfdf9959358d
Cameron Eagans <me@cweagans.net>
2023-04-12 12:19:28 -0600

    Use mirrornode data for exchange rate

--- dot_config/hammerspoon/hbar.lua
+++ dot_config/hammerspoon/hbar.lua
@@ -2,16 +2,18 @@ local mbar = hs.menubar.new(true, 'hbarprice')
 
 function refreshPrice()
 	mbar:setTitle("h: $....")
-	status, body = hs.http.get('https://api.coinbase.com/v2/exchange-rates?currency=HBAR')
+	status, body = hs.http.get('https://mainnet-public.mirrornode.hedera.com/api/v1/network/exchangerate')
 
 	if status ~= 200 then
 		return
 	end
 
 	hs.timer.doAfter(1, function()
-		prices = hs.json.decode(body)
-		usdprice = prices.data.rates.USD
-		mbar:setTitle(string.format("h: $%.4f", usdprice))
+		rates = hs.json.decode(body)
+		cent_equiv = rates.current_rate.cent_equivalent
+		hbar_equiv = rates.current_rate.hbar_equivalent
+		usd = (cent_equiv / hbar_equiv) / 100
+		mbar:setTitle(string.format("h: $%.4f", usd))
 	end)
 end
 
