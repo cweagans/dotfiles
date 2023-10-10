a06e323b3d53e831cd7e408943a7077f0ab9fb8c
Cameron Eagans <me@cweagans.net>
2023-10-10 11:19:46 -0600

    Make docusaurus more tolerable

--- dot_config/tamperish/docusaurus_dark.js
+++ dot_config/tamperish/docusaurus_dark.js
@@ -1,9 +1,27 @@
-  if (document.getElementById('__docusaurus')) {
-    console.log('docusaurus found');
+// Docusaurus is a constant annoyance that doesn't respect my gd preferences.
+if (document.getElementById('__docusaurus')) {
+  console.log('docusaurus found');
 
-    if (localStorage.getItem('theme') != 'dark') {
-      console.log('overriding theme and reloading the page');
-      localStorage.setItem('theme', 'dark');
-      location.reload();
-    }
+  var desiredMode = 'dark';
+  if (matchMedia('(prefers-color-scheme: light)').matches) {
+    desiredMode = 'light';
   }
+
+  // Just respect my gd preferences.
+  if (localStorage.getItem('theme') != desiredMode) {
+    console.log('overriding theme and reloading the page');
+    localStorage.setItem('theme', desiredMode);
+    location.reload();
+  }
+
+  // When the color mode changes, respect my gd preferences.
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
+    console.log('color mode changed. reloading the page.');
+    location.reload();
+  });
+
+  // The light/dark mode toggle is not necessary. Just respect my gd preferences.
+  document.querySelectorAll('[class^="colorModeToggle"],[class*=" colorModeToggle"]').forEach(function(el) {
+    el.style.display = 'none';
+  })
+}
