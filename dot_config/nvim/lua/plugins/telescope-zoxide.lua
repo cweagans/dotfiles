53f24b310e4d69e75e403d719b44e3cc6e0f674e
Cameron Eagans <me@cweagans.net>
2024-01-08 14:14:48 -0700

    Remove dashboard and add zoxide jump keybinding

--- dot_config/nvim/lua/plugins/telescope-zoxide.lua
+++ dot_config/nvim/lua/plugins/telescope-zoxide.lua
@@ -23,20 +23,28 @@ return {
     end,
   },
   {
-    "nvimdev/dashboard-nvim",
-    optional = true,
-    opts = function(_, opts)
-      local zoxide = {
-        action = "Telescope zoxide list",
-        desc = " Jump to directory",
-        icon = "",
-        key = "z",
-      }
-
-      zoxide.desc = zoxide.desc .. string.rep(" ", 43 - #zoxide.desc)
-      zoxide.key_format = "  %s"
-
-      table.insert(opts.config.center, 3, zoxide)
-    end,
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>Z"] = { "<cmd>Telescope zoxide list<cr>", "Zoxide jump" },
+      },
+    },
   },
+  --   {
+  --     "nvimdev/dashboard-nvim",
+  --     optional = true,
+  --     opts = function(_, opts)
+  --       local zoxide = {
+  --         action = "Telescope zoxide list",
+  --         desc = " Jump to directory",
+  --         icon = "",
+  --         key = "z",
+  --       }
+
+  --       zoxide.desc = zoxide.desc .. string.rep(" ", 43 - #zoxide.desc)
+  --       zoxide.key_format = "  %s"
+
+  --       table.insert(opts.config.center, 3, zoxide)
+  --     end,
+  --   },
 }
